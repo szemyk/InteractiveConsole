aea8f94c73709569cc93e64db12be78327a6fcb8
szemyxz <psorbal@gmail.com>
2016-04-12 16:14:56 +0200

    Initial commit

---
+++ src/main/java/com/epam/console/Main.java
@@ -0,0 +1,11 @@
+package com.epam.console;
+
+/**
+ * Main class for interactive console application
+ */
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("For initial commit");
+    }
+}
