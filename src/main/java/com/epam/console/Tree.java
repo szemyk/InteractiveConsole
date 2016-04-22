2832af112cc6bc3a0a96d76e772351157bea8528
szemyxz <psorbal@gmail.com>
2016-04-22 14:41:59 +0200

    Create methods generating directory tree structure in Tree class

--- src/main/java/com/epam/console/Tree.java
+++ src/main/java/com/epam/console/Tree.java
@@ -1,5 +1,7 @@
 package com.epam.console;
 
+import java.io.File;
+
 /**
  * Class for tree command.
  * User can display the directory structure of current working directory
@@ -13,11 +15,40 @@ class Tree implements Command{
     }
 
     public void executeCommand() {
-        System.out.println("tree");
-        //to do something with command tree
+        File currDir = new File(Main.parameter.getPath());
+        System.out.print(printTree(currDir));
     }
 
     public boolean endLoop() {
         return true;
+    }
+
+    private String printTree(File directory){
+        int branch = 0;
+        StringBuilder sb = new StringBuilder();
+        printDirectoryTree(directory, branch, sb);
+        return sb.toString();
+    }
+
+    private void printDirectoryTree(File directory, int branch, StringBuilder sb){
+        sb.append(getBranch(branch));
+        sb.append(directory.getName());
+        sb.append("\n");
+        File [] files = directory.listFiles();
+        if(files != null) {
+            for (File file : files) {
+                if (file.isDirectory() && !file.isHidden()) {
+                    printDirectoryTree(file, branch + 1, sb);
+                }
+            }
+        }
+    }
+
+    private static String getBranch(int branch) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < branch; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
     }
 }
